cdb4006ec52785f341f13489906039c3baa21df7
Michael Sol <Sol.mike.j@gmail.com>
2023-11-24 09:35:11 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,137 @@
+import logo from './logo.svg';
+import './App.css';
+
+/*
+This Space intentionally left blank
+*/
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p className="Page-title">
+          Moving sale! Prices negotiable! 
+        </p>
+        <p className="Contact-info"> 
+          Email: <a href="mailto:sol.mike.j@gmail.com">Sol.Mike.J@gmail.com</a> <br></br>
+          Text: (443) 896 - 7051 <br></br>
+          Apt: 2009
+        </p>
+        <p>
+          <img src={"test.png"} />
+          <a href="https://www.amazon.com/gp/product/B001KW0D0Y/ref=ppx_yo_dt_b_search_asin_title?ie=UTF8&psc=1"><br></br>
+            Dresser - $120
+          </a>
+        </p>
+        <p className="Item">
+          <img src={logo}/>
+          Steelcase Leap - $250
+        </p>
+        <p className="Item">
+          <img src={logo}/>
+          <a href="https://www.amazon.com/gp/product/B07LG11F5S/ref=ppx_yo_dt_b_search_asin_title?ie=UTF8&psc=1">
+            Saddle Chair - $100
+          </a>
+        </p>
+        <p className="Item">
+          <a href="https://www.amazon.com/gp/product/B08GF9K41Z/ref=ppx_yo_dt_b_search_asin_title?ie=UTF8&th=1">
+            Large Standing Computer Desk - $200
+          </a>
+        </p>
+        <p className="Item">
+          <a href="https://www.amazon.com/gp/product/B09Q8WJCTM/ref=ppx_yo_dt_b_search_asin_title?ie=UTF8&th=1">
+            Small Computer Desk - $35
+          </a>
+        </p>
+        <p className="Item">
+          <a href="https://www.amazon.com/gp/product/B00BTKAPUU/ref=ppx_yo_dt_b_search_asin_title?ie=UTF8&psc=1">
+            Air Purifier - $120
+          </a>
+        </p>
+        <p className="Item">
+          <a href="https://www.amazon.com/gp/product/B08C6HR53B/ref=ppx_yo_dt_b_search_asin_title?ie=UTF8&psc=1">
+            Miele Vacuum Cleaner - $375
+          </a>
+        </p>
+        <p className="Item">
+          <a href="https://www.amazon.com/gp/product/B00BTKAPUU/ref=ppx_yo_dt_b_search_asin_title?ie=UTF8&psc=1">
+            Bookshelf - $70
+          </a>
+        </p>
+        <p className="Item">
+          <a href="https://www.amazon.com/gp/product/B003QCI4GG/ref=ppx_yo_dt_b_search_asin_title?ie=UTF8&psc=1">
+            Inversion Table - $80
+          </a>
+        </p>
+        <p>
+          <a href="https://www.amazon.com/gp/product/B095X5T99T/ref=ppx_yo_dt_b_search_asin_title?ie=UTF8&th=1">
+            Gaming Monitor - $600
+          </a>
+        </p>
+        <p>
+          <a href="https://www.amazon.com/gp/product/B089HPRSJ4/ref=ppx_yo_dt_b_search_asin_title?ie=UTF8&th=1">
+            Ultrawide Monitor - $130
+          </a>
+        </p>
+        <p className="Item">
+          <a href="https://www.amazon.com/gp/product/B0074CDG6C/ref=ppx_yo_dt_b_search_asin_title?ie=UTF8&psc=1">
+            Rice Cooker - $120
+          </a>
+        </p>
+        <p className="Item">
+          <a href="https://www.amazon.com/gp/product/B003VWMS5E/ref=ppx_yo_dt_b_search_asin_title?ie=UTF8&psc=1">
+            Large Kitchen Trash can - $45
+          </a>
+        </p>
+        <p>
+          <a href="https://www.amazon.com/gp/product/B07PCXZ4CP/ref=ppx_yo_dt_b_search_asin_title?ie=UTF8&th=1">
+            Small Trash Can - $20
+          </a>
+          <a href="https://www.amazon.com/gp/product/B0B29JS5WP/ref=ppx_yo_dt_b_search_asin_title?ie=UTF8&psc=1">
+            Coat Rack - $10
+          </a>
+        </p>
+        <p className="Item">
+          <a href="https://www.amazon.com/gp/product/B09KC7Q4YF/ref=ppx_yo_dt_b_search_asin_title?ie=UTF8&psc=1">
+            Automatic Cat Litter Box - $300
+          </a>
+        </p>
+        <p className="Item">
+          <a href="https://www.amazon.com/gp/product/B098JK84W5/ref=ppx_yo_dt_b_search_asin_title?ie=UTF8&th=1">
+            Automatic Cat Feeder - $50
+          </a>
+        </p>
+        <p className="Item">
+          <a href="https://www.amazon.com/gp/product/B08Y5LNF87/ref=ppx_yo_dt_b_search_asin_title?ie=UTF8&psc=1">
+            Automatic Cat Water Fountain - $15
+          </a>
+        </p>
+        <p>
+          <a href="https://www.amazon.com/gp/product/B0794T79KM/ref=ppx_yo_dt_b_search_asin_title?ie=UTF8&psc=1">
+            Large Cat Tree - $30
+          </a>
+        </p>
+        <p>
+          <a href="https://www.amazon.com/gp/product/B08F5BBZ2M/ref=ppx_yo_dt_b_search_asin_title?ie=UTF8&th=1">
+            Medium Cat Tree - $15
+          </a>
+        </p>
+        <p>
+          <a href="https://www.amazon.com/gp/product/B08CY5VR5W/ref=ppx_yo_dt_b_search_asin_title?ie=UTF8&psc=1">
+            2 Cat Boxes - $10 each or $15 for both
+          </a>
+        </p>
+        <p>
+          <a href="https://www.amazon.com/gp/product/B07WDDJNCR/ref=ppx_yo_dt_b_search_asin_title?ie=UTF8&th=1">
+            Cat Cactus - $10
+          </a>
+        </p>
+
+        <hr></hr>
+      </header>
+    </div>
+  );
+}
+
+export default App;
